c1dd0302c84cbd846ec717f84a5d15f1bcb087ba
Nikunja0611 <sonawanenikunja@gmail.com>
2025-01-15 01:15:24 +0530

    Adding Fronted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,14 @@
-import React from 'react'
-import TextSummarizer from './components/TextSummarizer'
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Summarizer from "./components/Summarizer";
 
 function App() {
   return (
-    <div className="w-screen min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-          <h1 className="text-4xl font-bold mb-8 text-center text-gray-800">Text Summarization Platform</h1>
-          <TextSummarizer />
-        </div>
-      </div>
+    <div className="flex h-screen">
+      <Sidebar />
+      <Summarizer />
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
